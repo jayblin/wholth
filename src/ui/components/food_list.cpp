ef9ea0ebc641d65b8a414f3183049ada2739728c
Grigoriy Shaburov <hiehiohi@gmail.com>
2025-04-26 19:08:14 +0500

    Removed gui stuff

--- src/ui/components/food_list.cpp
+++
@@ -1,9 +0,0 @@
-#include "ui/components/food_list.hpp"
-#include "wholth/entity/locale.hpp"
-#include <cstddef>
-#include <tuple>
-
-/* template<size_t Size> */
-/* ui::components::FoodList<Size>::FoodList( */
-
-
--- src/ui/imgui.cpp
+++
@@ -1,6 +0,0 @@
-#include "vk/vk.hpp"
-
-void check_no_msg(VkResult result)
-{
-    vk::check(result, "[No message]");
-}
