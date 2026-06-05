355a9ca957d2b2f2ef82fb6cf4a60f48e992437b
Grigoriy Shaburov <hiehiohi@gmail.com>
2026-06-05 20:03:31 +0500

    can update consumed_at now

--- sql_statements/consumption_log_update.sql
+++ sql_statements/consumption_log_update.sql
@@ -2,5 +2,8 @@
 -- ?1;INT;is_valid_id;Невалидный идентифкатор лога!
 -- ?2;INT;is_valid_id;Невалидный идентифкатор пользователя!
 -- ?3;INT;
-UPDATE consumption_log SET mass = ?3
+-- ?4;TEXT;
+UPDATE consumption_log SET
+    mass = coalesce(?3, mass),
+    consumed_at = coalesce(?4, consumed_at)
 WHERE id = ?1 AND user_id = ?2
